28ada2172161897298394d1bd0de67fd502903e2
Dayeeta Chakravorty <102747185+Dayeeta07@users.noreply.github.com>
2024-02-24 00:25:05 +0530

    Update donate.js

--- donate.js
+++ donate.js
@@ -1,30 +1,27 @@
-
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBRK9qc7XlYo3jFFfZaeLKPbkIXKv2l_aM",
-    authDomain: "nourish-together.firebaseapp.com",
-    projectId: "nourish-together",
-    storageBucket: "nourish-together.appspot.com",
-    messagingSenderId: "168163500855",
-    appId: "1:168163500855:web:0e10c571e09d9927ebb0b2"
-  };
+  apiKey: "AIzaSyBRK9qc7XlYo3jFFfZaeLKPbkIXKv2l_aM",
+  authDomain: "nourish-together.firebaseapp.com",
+  projectId: "nourish-together",
+  storageBucket: "nourish-together.appspot.com",
+  messagingSenderId: "168163500855",
+  appId: "1:168163500855:web:0e10c571e09d9927ebb0b2"
+};
+
+firebase.initializeApp(firebaseConfig);
+var NourishTogetherDB = firebase.database().ref('Nourish together');
+
+document.getElementById('NourishTogether').addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  var Name = getElementByVal("Name");
+  var Email = getElementByVal("Email");
+  var Contact = getElementByVal("Contact");
+  var Address = getElementByVal("Address");
   
-  firebase.initializeApp(firebaseConfig);
-  var NourishTogetherDB =firebase.database().ref('Nourish together');
+  console.log(Name, Email, Contact, Address);
+}
 
-  document.getElementById('NourishTogether').addEventListener("submit", submitForm);
-
-  function(submitForm(e)){
-    e.preventDefault();
-    var Name = getElementByval("Name");
-    var Email = getElementByval("Email");
-    var Contact = getElementByval("Contact");
-    var Address = getElementByval("Address");
-    
-    console.log(Name,Email,Contact,Address);
-  }
-
-  const getElementByval=(id)=>{
-    return document.getElementById(id).value
-
-  }
+const getElementByVal = (id) => {
+  return document.getElementById(id).value;
+}
