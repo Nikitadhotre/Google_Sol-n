4b084ec9d0a8ac96fff35aa1713de5cb94657a07
sakshibidwe <sakshibidwe3@gmail.com>
2024-02-23 23:53:44 +0530

    donate.js

--- donate.js
+++ donate.js
@@ -1,3 +1,30 @@
-document.getElementById("donation").addEventListener("submit",(event)=>){
-    event.preventDefault
-}
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBRK9qc7XlYo3jFFfZaeLKPbkIXKv2l_aM",
+    authDomain: "nourish-together.firebaseapp.com",
+    projectId: "nourish-together",
+    storageBucket: "nourish-together.appspot.com",
+    messagingSenderId: "168163500855",
+    appId: "1:168163500855:web:0e10c571e09d9927ebb0b2"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  var NourishTogetherDB =firebase.database().ref('Nourish together');
+
+  document.getElementById('NourishTogether').addEventListener("submit", submitForm);
+
+  function(submitForm(e)){
+    e.preventDefault();
+    var Name = getElementByval("Name");
+    var Email = getElementByval("Email");
+    var Contact = getElementByval("Contact");
+    var Address = getElementByval("Address");
+    
+    console.log(Name,Email,Contact,Address);
+  }
+
+  const getElementByval=(id)=>{
+    return document.getElementById(id).value
+
+  }
